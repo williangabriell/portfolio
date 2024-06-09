2539a9f7b61ffced1bed5f7b196b17786b144cdd
Willian Gabriel <willianlopes20@hotmail.com>
2024-06-09 02:46:06 -0300

    aninhando as rotas

--- src/pages/Projetos/index.jsx
+++ src/pages/Projetos/index.jsx
@@ -1,16 +1,9 @@
-import Header from '../../components/Header'
-import Footer from '../../components/Footer'
-import Container from '../../components/Container'
 
 function Projetos() {
     return (
-        <>
-            <Header />
-            <Container>
-                <h2>Projetos</h2>
-            </Container>
-            <Footer />
-        </>
+        <section>
+            <h2>Projetos</h2>
+        </section>
     )
 }
 
--- src/pages/Sobre/index.jsx
+++ src/pages/Sobre/index.jsx
@@ -1,16 +1,9 @@
-import Header from '../../components/Header'
-import Footer from '../../components/Footer'
-import Container from '../../components/Container'
 
 function Sobre() {
     return (
-        <>
-            <Header />
-            <Container>
-                <h2>Sobre</h2>
-            </Container>
-            <Footer />
-        </>
+        <section>
+            <h2>Sobre</h2>
+        </section>
     )
 }
 
