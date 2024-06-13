13f6add482d86eaac9c793d6d3b2cc15bed7f4d8
Willian Gabriel <willianlopes20@hotmail.com>
2024-06-13 12:05:54 -0300

    ajustado link de download do curriculo

--- src/pages/Sobre/index.jsx
+++ src/pages/Sobre/index.jsx
@@ -42,7 +42,7 @@ function Sobre() {
                 </div>
             </div>
             <div>
-                <a href="src/assets/curriculo.pdf" download="Currículo - Willian Gabriel" className={styles.button}>Baixe meu Currículo</a>
+                <a href= "https://drive.google.com/uc?export=download&id=1aL_N1jt9qMHg1Xh6NjD1IW4PKl7jraJ1" download=" Currículo - Willian Gabriel" className={styles.button}>Baixe meu Currículo</a>
             </div>
         </section>
     )
